f0693329cb7588e488dac96f6d220b2cee37b9a7
manbodhvikram <manbodhvikram@gmail.com>
2023-08-25 01:16:22 +0530

    submitting code-Fri Aug 25 2023 01:16:19 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip"
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>
+      <Tooltip text="This is a tooltip">Hover over me!</Tooltip>
     </div>
-  )
+
+<div className="">
+      <Tooltip text="This is a anothre tooltip">Hover over me to see another tooltip!</Tooltip>
+</div>
+</div>
+  );
 }
 
-export default App
+export default App;
+// In this example, I've used arrow functions to define the functional components and followed the same approach as the previous examples to create the Tooltip component using useState.
+
+
+
+
+
